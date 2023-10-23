e37739ada15e7e2dfe8bbcad48ba8ede5306ce78
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-10-23 12:37:13 +0100

    styled and edited html, also made js plan

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+
+Task:
+- create function, attach to button
+- create loop that will loop through each field and collect field values
+- join all values into a single string
+- display values in textResult field
+
+*/
